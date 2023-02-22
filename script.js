1d2eb296984497e6476ee2c270e3f7d3ab1df5a2
SelloDaGod <Marcel.mk1577@gmail.com>
2023-02-21 19:25:10 -0600

    New About Me questions

---
+++ script.js
@@ -0,0 +1,52 @@
+function MyFunction1() {
+    let answer = prompt("Answer1")
+    if (answer == "yes") {
+        alert("Correct!")
+    } else {
+        alert("Incorrect!")
+
+    }
+}
+function MyFunction2() {
+    let answer = prompt("Answer2")
+    if (answer == "yes") {
+        alert("Correct!")
+    } else {
+        alert("Incorrect!")
+
+    }
+}
+function MyFunction3() {
+    let answer = prompt("Answer3")
+    if (answer == "no") {
+        alert("Correct")
+
+    } else {
+        alert("Incorrect!")
+
+    }
+}
+function MyFunction4() {
+    let answer = prompt("Answer4")
+    if (answer == "no") {
+        alert("Correct!")
+
+    } else {
+        alert("Incorrect!")
+
+    }
+}
+function MyFunction5() {
+    let answer = prompt("Answer5")
+    if (answer == "yes") {
+        alert("Correct!")
+
+    } else {
+        alert("Incorrect")
+
+    }
+}
+function MyFunction6(){
+   let answer = prompt("Name?") 
+   alert("Thank you for getting to know me " + answer)
+}
